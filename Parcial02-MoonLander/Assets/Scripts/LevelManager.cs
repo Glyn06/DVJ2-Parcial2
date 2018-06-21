2228cf3a0084c2953f096b112f756feb5742b382
Unknown <jquintanamartin@hotmail.com>
2018-06-21 16:17:27 -0300

    Generador de terreno

---
+++ Parcial02-MoonLander/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public List<GameObject> Terrains;
+
+    private int randomNum;
+
+	// Use this for initialization
+	void Start () {
+        randomNum = Random.Range(0,Terrains.Count);
+        Instantiate(Terrains[randomNum],transform.position,Quaternion.identity,transform);
+        Debug.Log(randomNum);
+	}
+}
